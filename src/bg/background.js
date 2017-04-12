9557226a5749ee67d1e188e7aca661d25054a459
John Morone <jmorone@princeton.edu>
2017-04-12 05:39:01 -0400

    Toggle Page Features Prototype
    Simple Prototype allowing a few page features to be enabled/disabled
    with toggle buttons in the extension pop-up:
    Images
    Scripts
    Styles

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,6 +4,11 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+var currTab;
+
+var styles = true;
+var images = true;
+var scripts = true;
 
 //example of using a message handler from the inject scripts
 chrome.extension.onMessage.addListener(
@@ -11,3 +16,81 @@ chrome.extension.onMessage.addListener(
   	chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+function reloadCurrentTab() {  
+  chrome.tabs.reload();
+}
+
+function setCurrentPagePermission(feature, setting) {  
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    currTab = tabs[0].url + "/";
+    var domain = currTab.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+    domain = '*://'+domain+'/*';
+    alert(domain);
+    feature.set({
+      'primaryPattern': domain,
+      'setting': setting
+    });
+    reloadCurrentTab();
+  });
+}
+
+function disableImages() {  
+  setCurrentPagePermission(chrome.contentSettings.images, 'block');
+}
+
+function enableImages() {  
+  setCurrentPagePermission(chrome.contentSettings.images, 'allow');
+}
+
+function disableScripts() {  
+  setCurrentPagePermission(chrome.contentSettings.javascript, 'block');
+}
+
+function enableScripts() {  
+  setCurrentPagePermission(chrome.contentSettings.javascript, 'allow');
+}
+
+function disableStyles(){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    alert(tabs[0].id);
+    chrome.tabs.sendMessage(tabs[0].id, {command: 'modify_styles', setting: 'disable'}, 
+    function(response) {
+      //alert(response.numStyles);
+    });
+  });
+}
+
+function enableStyles(){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    alert(tabs[0].id);
+    chrome.tabs.sendMessage(tabs[0].id, {command: 'modify_styles', setting: 'enable'}, 
+    function(response) {
+      //alert(response.numStyles);
+    });
+  });
+}
+
+function toggleScripts(){
+  if(scripts) disableScripts();
+  else       enableScripts();
+  scripts = !scripts;
+}
+
+function toggleImages(){
+  if(images) disableImages();
+  else       enableImages();
+  images = !images;
+}
+
+function toggleStyles(){
+  if(styles) disableStyles();
+  else       enableStyles();
+  styles = !styles;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('toggleJS').onclick = toggleScripts;
+    document.getElementById('toggleIMG').onclick = toggleImages;
+    document.getElementById('toggleStyles').onclick = toggleStyles;
+}); 
