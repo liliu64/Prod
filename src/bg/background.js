2da084d90a5be1b39c89486f265ac363b7aeb0d3
jdcase <jdcase66@gmail.com>
2017-04-21 12:39:19 -0400

    getData()
    Added function to return local version of history data

--- src/bg/background.js
+++ src/bg/background.js
@@ -19,6 +19,13 @@ chrome.extension.onMessage.addListener(
     sendResponse();
   });
 
+function getData() {
+	var data = {};
+	for (key in History) {
+		data[key] = History[key][0];
+	}
+	return data;
+}
 function Update(date, tabId, url) {
   
     if (!url) {
