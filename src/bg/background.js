c49f65784ebba0c2695054c671190233160a3dd2
John Morone <jmorone@princeton.edu>
2017-04-21 12:59:27 -0400

    Overlay Injection
    Inject HTML and CSS into web pages to allow for our overlay messages.

--- src/bg/background.js
+++ src/bg/background.js
@@ -89,8 +89,27 @@ function toggleStyles(){
   styles = !styles;
 }
 
+function triggerOverlay(type, url, time){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      chrome.tabs.sendMessage(tabs[0].id, {command: 'display_message', 
+                            setting: type,
+                            url: url,
+                            time: time}, 
+      function(response) {
+    });
+  });
+}
+
+function triggerWarning(){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+        var domain = tabs[0].url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+        triggerOverlay('warning', domain, '9');
+  });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('toggleJS').onclick = toggleScripts;
     document.getElementById('toggleIMG').onclick = toggleImages;
     document.getElementById('toggleStyles').onclick = toggleStyles;
+    document.getElementById('triggerWarning').onclick = triggerWarning;
 }); 
