670966ff74db627a0b944cd78d28a6d957e695a8
Edgar <mredgaryu@gmail.com>
2017-04-25 21:53:33 -0400

    Adjust data structure for options page

--- src/options_custom/js/options.js
+++ src/options_custom/js/options.js
@@ -1,102 +1,167 @@
 // window.localStorage.clear();
 
+/* ----------------
+ * Functions that directly affect the html page. 
+ ------------------ */
+
+// Initialize datatable by requesting data from chrome.storage
 $(document).ready(function() {
+	// Table formatting
+ 	// $( "#tabs" ).tabs();
+ 	// $( "#tabs" ).addClass('ui-tabs-vertical ui-helper-clearfix');
+	// $( "#tabs" ).tabs().addClass( "ui-tabs-vertical ui-helper-clearfix" );
+	// $( "#tabs li" ).removeClass( "ui-corner-top" ).addClass( "ui-corner-left" );
+
 	// Initialize table
 	$('#example').dataTable( {
 		// data: getdata(),
-		columns: [
-		{ data: 'url' },
-		{ data: 'alarm' },
-		{ data: 'etc' }
-		]
+		// columns: [
+		// { data: 'url' },
+		// { data: 'alarm' },
+		// { data: 'etc' }
+		// ]
 	} );
-	requestTableUpdate();
+	updateTable();
 
  	//Add row
 	$('#addrow').click(addRow);
-
  	// Select rows
 	$('#example tbody').on( 'click', 'tr', function () {
 		$(this).toggleClass('selected');
 	} );
-
-	// $('#button').click( function () {
-	// 	alert( table.rows('.selected').data().length +' row(s) selected' );
-	// 	} );
-
 	// Remove selected rows
 	$('#removerow').click(deleteRow);
 
+	// var tabsFn = (function() {
+
+	// 	function init() {
+	// 		setHeight();
+	// 	}
+
+	// 	function setHeight() {
+	// 		var $tabPane = $('.tab-pane'),
+	// 		tabsHeight = $('.nav-tabs').height();
+
+	// 		$tabPane.css({
+	// 			height: tabsHeight
+	// 		});
+	// 	}
+
+	// 	$(init);
+	// })();
+
 } );
 
-function requestTableUpdate() {
-	// var dataSet = [];
-	
-	// for (var key in localStorage) {
-	// 	dataSet.push((JSON.parse(localStorage[key])));
-	// }
-	$('#loadingSpinner').css('visibility', 'visible');
-	chrome.storage.sync.get(null, function(data) {
-		// console.log(data);
+
+
+
+/* ----------------
+ * Helper functions. 
+ ------------------ */
+
+// Update the datatable
+function updateTable() {
+	// $('#loadingSpinner').css('visibility', 'visible');
+
+	chrome.storage.sync.get('History', function(data) {
+		var History = {};
+
+  		History = data['History'];
+
 		var dataSet = [];
-		for (url in data) {
-			dataSet.push(data[url]);
+
+		// Convert History into array format (with 3 values)
+		for (key in History) {
+			var entry = [];
+
+
+			// Clean up key
+			var cleankey = key.substring(4, key.length - 2)
+			entry.push(cleankey, History[key][2], History[key][3]);
+
+			dataSet.push(entry);
 		}
-		updateTable(dataSet);
+
+		// console.log(dataSet);
+
+		// Update datatable by first erasing it and then re-initializing it
+		$('#example').dataTable().fnClearTable();
+
+		if (dataSet != null && dataSet.length > 0) {
+			$('#example').dataTable().fnAddData(dataSet);
+		}
+		// $('#loadingSpinner').css('visibility', 'hidden');
+
 	} );
 
-	// return dataSet
 }
 
-// data should be an array of objects
-function updateTable(data) {
-	$('#example').dataTable().fnClearTable();
-
-	if (data != null && data.length > 0)
-		$('#example').dataTable().fnAddData(data);
-	$('#loadingSpinner').css('visibility', 'hidden');
-}
-
+// Add new row, with values given by html form
 function addRow() {
 	// Form values
 	var newurl = $('#form-url').val();
-	var newalarm = $('#form-alarm').val();
+	var newalarm = Number($('#form-alarm').val());
 	var newetc = $('#form-etc').val();
 
-	var newentry = {
-		"url": newurl,
-		"alarm": newalarm,
-		"etc": newetc
-	};
-	var objRow = {};
-	objRow[newurl] = newentry;
+	//Check url from form
+	if (!newurl) {
+		console.log("No URL found!"); //**** put red warning msg
+		return;
+	}
 
-	// Insert form values into localStorage
-	// window.localStorage.setItem(newurl, JSON.stringify(newentry));
-	chrome.storage.sync.set( objRow );
+	chrome.storage.sync.get('History', function(data) {
+		var History;
 
-	// Change addmsg in html
-	var addmsg = "New URL added. [URL: " + newurl + ", Alarm: " + newalarm + 
-		", Etc: " + newetc + "]";
-	$('#addmsg').html(addmsg);
-	$('#addmsg').css('visibility', 'visible');
-	// setTimeout(fade_out, 10000);
+		if (data['History'] == null) {
+  			History = {};
+  			console.log("Setting default site");
+  		} else {
+  			History = data['History'];
+  		}
+  		
+  		// Parse url
+  		newurl = "https://" + newurl + "/";
+  		var domain = newurl.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+    	domain = '*://'+domain+'/*';
 
-	// Update table
-	requestTableUpdate();
+		History[domain] = [0,0, newalarm, newetc];
+
+  		// Change addmsg in html
+  		var addmsg = "New URL added. [URL: " + domain + ", AlarmRule: " + newalarm + 
+  		", AlarmType: " + newetc + "]";
+		$('#addmsg').html(addmsg);
+		$('#addmsg').css('visibility', 'visible');
+
+		chrome.storage.sync.set({'History': History}, function() {
+			updateTable();
+		});
+
+	} );
 }
-
 
 function deleteRow() {
 	var todelete = [];
 	todelete = $('#example').DataTable().rows('.selected').data();
 	var length = todelete.length;
 
-	for (i = 0; i < length; i++) {
-		var deleteurl = todelete[i]["url"];
-		// localStorage.removeItem(deleteurl);
-		chrome.storage.sync.remove(deleteurl);
-	}
+	chrome.storage.sync.get('History', function(data) {
+		var History =  data['History'];
 
-	requestTableUpdate();
+		for (i = 0; i < length; i++) {
+			// Get url
+			var domain = todelete[i][0];
+			domain = '*://'+domain+'/*';
+			
+			delete History[domain];
+			
+		}
+		// console.log(History);
+
+		chrome.storage.sync.set({'History': History}, function() {
+			updateTable();
+		});
+
+	} );
+
+	
 }
