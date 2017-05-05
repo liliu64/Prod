6e45b46f034f8eece90417eb4ca837f2e2bd3245
Linda Liu <Zhiyu@nat-oitwireless-inside-vapornet100-a-7009.Princeton.EDU>
2017-05-05 14:37:10 -0400

    Cleaned up progress.js
    Cleaned up the page

--- src/browser_action/progress.js
+++ src/browser_action/progress.js
@@ -7,38 +7,40 @@
  */
 
 var bgPage = chrome.extension.getBackgroundPage(); // background.js
-var allData;
-var numAlarms = 0;
+var numAlarms = 0; // number of alarms and thus progress bars on the page
 
+// Create and draw all progress bars
 function updateBars() {
 	// Get all history information
 	// URL: [time, "LastAccess", [alarm0, alarm1], ["type1", "type2"], ["d/w", "d/w"]];
-	allData = bgPage.History;
+	var allData = bgPage.History; // all the data from History
 	var allAlarms = {}; // all the alarms that exist
+
+	// Create progress bars for each valid alarm for each url
 	for (url in allData) {
 		for (alarm in allData[url][2]) {
 			if (allData[url][2][alarm] != 0) {
 				// will pass this array to create()
 				// consists of [url, TimeSpent, TimeOfAlarm, AlarmType]
 				var newAlarm = [url, allData[url][0], allData[url][2][alarm], allData[url][3][alarm]];
-				create(newAlarm);
+				createBar(newAlarm);
 				numAlarms += 1;
 			}
 		}
 	}
 }
 
+// Call function once
 updateBars();
 
-function create(alarm) {
+// Create a progress bar given an alarm
+function createBar(alarm) {
 	var svg = d3.select('.progress')
 		.append('svg')
 		.attr('height', 50)
 		.attr("id", "Alarm_Progress");
 
-	var colorScale = d3.scale.ordinal()
-		.range(['brown']);
-
+	// Background rectangle, background of entire progress bar
 	svg.append('rect')
 		.attr('class', 'bg-rect')
 		.attr('rx', 10)
@@ -49,6 +51,7 @@ function create(alarm) {
 		.attr('height', 10)
 		.attr('width', 400);
 
+	// Text for the progress bar
 	svg.append('text')
 		.attr("y", 5)
 		.attr("x", 0)
@@ -67,6 +70,7 @@ function create(alarm) {
     		return urlName;
     	});
 
+    // Progress rectangle, colored part of progress bar
 	var progress = svg.append('rect')
 					.attr('class', 'progress-rect')
 					.attr('rx', 10)
@@ -77,7 +81,7 @@ function create(alarm) {
 					.attr('height', 10)
 					.attr('width', 0);
 
-
+	// Fill the progress bar color up to amount of time spent
 	progress.transition()
 		.duration(1000)
 		.attr('width', function(){
