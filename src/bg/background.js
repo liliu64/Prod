dea8b9a6442517d2e869d2f02c696ac1e333ad07
John Morone <jmorone@princeton.edu>
2017-05-05 16:17:59 -0400

    Fixed Disable and OVerlay Behavior
    On first trigger, content settings are updated and page is refreshed. On
    subsequent triggers, only the overlay is shown. The first refresh
    triggers the rule again, causing the overlay to be shown the first time.

--- src/bg/background.js
+++ src/bg/background.js
@@ -62,101 +62,102 @@ function getData() {
 	
 // }
 
-function Activate(url) {
+function Activate(url, tabId) {
+  chrome.storage.sync.get('History', function(data) {
+    if (data['History'] == null) {
+        History = {"*://google.com/*": [0, "", [0], [""], [""]]};
+      } else {
+        History = data['History'];
+      }
+      
+      if (!url) {
+        return;
+      }
+      var domain = url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
+      if (domain.substring(0,4) == 'www.') domain = domain.substring(4,domain.length);
+    domain = '*://'+domain+'/*';
+    var date = new Date();
+    if (lastActive in History) {
+      History[lastActive][0] += date.getTime() - new Date(History[lastActive][1]).getTime();
+      History[lastActive][1] = "";
+    }
 
-	chrome.storage.sync.get('History', function(data) {
-		if (data['History'] == null) {
-  			History = {"*://google.com/*": [0, "", [0], [""], [""]]};
-  		} else {
-  			History = data['History'];
-  		}
-  		
-	    if (!url) {
-	    	return;
-	    }
-	    var domain = url.match(/^[\w-]+:\/{2,}\[?([\w\.:-]+)\]?(?::[0-9]*)?/)[1];
-	    if (domain.substring(0,4) == 'www.') domain = domain.substring(4,domain.length);
-		domain = '*://'+domain+'/*';
-		var date = new Date();
-		if (lastActive in History) {
-			History[lastActive][0] += date.getTime() - new Date(History[lastActive][1]).getTime();
-			History[lastActive][1] = "";
-		}
-
-		// bug catcher 
-		for (key in History) {
-			if (key[1] != "") {
-				key[0] = date.getTime() - new Date(key[1]).getTime();
-				key[1] = "";
-			}
-		}
+    // bug catcher 
+    for (key in History) {
+      if (key[1] != "") {
+        key[0] = date.getTime() - new Date(key[1]).getTime();
+        key[1] = "";
+      }
+    }
 
 
-		lastActive = domain;
-	    if (domain in History) {
-			History[domain][1] = date.toJSON();
-	    } else {
-	    	History[domain] = [0,date.toJSON(),[0], [""], [""]];
-	    }
+    lastActive = domain;
+      if (domain in History) {
+      History[domain][1] = date.toJSON();
+      } else {
+        History[domain] = [0,date.toJSON(),[0], [""], [""]];
+      }
 
-	    var website = domain.substring(4,domain.length - 2);
+      var website = domain.substring(4,domain.length - 2);
 
-	    
-	    var maxIndex = -1;
-	    var maxHours = 0; 
-	    for (var i = 0; i < History[domain][2].length; i++) {
-	    	var time = History[domain][2][i];
-	    	if (time > 0) {
-	    		//60000 ms per minute
-	    		if (History[domain][0] > time) {
-	    			var hours = time / (60 * 60 * 1000);
-	    			if (hours > maxHours) {
-	    				maxIndex = i;
-	    				maxHours = hours;
-	    			}
-		    	}
-	    	}
-	    }
-	    if (maxIndex != -1) {
-	    	var i = maxIndex;
-	    	switch (History[domain][3][i]) {
-				case "warning":
-					triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
-					break;
-				case "images":
-					disableImages(domain, true);
-					triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
-					break;
-				case "scripts":
-					disableScripts(domain, true);
-					triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
-					break;
-				case "styles":
-					chrome.tabs.getCurrent(function(tab) {
-						disableStyles(tab.id);
-					});
-					triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
-					break;
-				default:
-					break;
-			}
-	    			
-	    }
+      
+      var maxIndex = -1;
+      var maxHours = 0; 
+      for (var i = 0; i < History[domain][2].length; i++) {
+        var time = History[domain][2][i];
+        if (time > 0) {
+          //60000 ms per minute
+          if (History[domain][0] > time) {
+            var hours = time / (60 * 60 * 1000);
+            if (hours > maxHours) {
+              maxIndex = i;
+              maxHours = hours;
+            }
+          }
+        }
+      }
+      if (maxIndex != -1) {
+        var i = maxIndex;
+        switch (History[domain][3][i]) {
+        case "warning":
+          triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
+          break;
+        case "images":
+          chrome.contentSettings.images.get({primaryUrl: url}, function (details) {
+            if(details.setting == 'allow') disableImages(domain, false);
+            else triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
+          });
+          break;
+        case "scripts":
+          chrome.contentSettings.javascript.get({primaryUrl: url}, function (details) {
+            if(details.setting == 'allow') disableScripts(domain, true);
+            else triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]);
+          });
+          break;
+        case "styles":
+          disableStyles(tabId, 
+          triggerOverlay(History[domain][3][i],website, hours + ' hours', History[domain][4][i]));
+          break;
+        default:
+          break;
+      }   
+      }
 
-	    // Non array version of alarms
-	    // if (History[domain][2] > 0) {
-	    // 	if (History[domain][0] > 60000 * History[domain][2]) {
-		   //  	triggerOverlay(History[domain][3],website, History[domain][2].toString() + ' minutes');
-		   //  }
-	    // }
+      // Non array version of alarms
+      // if (History[domain][2] > 0) {
+      // 	if (History[domain][0] > 60000 * History[domain][2]) {
+       //  	triggerOverlay(History[domain][3],website, History[domain][2].toString() + ' minutes');
+       //  }
+      // }
 
-	    chrome.storage.sync.set({'History': History});
-	} );
+      chrome.storage.sync.set({'History': History});
+  } );
 }
 
 function HandleUpdate(tabId, changeInfo, tab) {
-	Activate(changeInfo.url);
-	
+	if (changeInfo.status == 'complete'){
+    Activate(tab.url, tabId); 
+  }
 }
 
 function HandleActivated(activeInfo) {
@@ -164,7 +165,7 @@ function HandleActivated(activeInfo) {
 	var url;
 	chrome.tabs.get(tabId, function(tab) {
 		url = tab.url;
-		Activate(url);
+		Activate(url, tabId);
 	});
 }
 
@@ -199,36 +200,36 @@ function setPagePermission(matchPattern, feature, setting, reload) {
   if(reload) { reloadCurrentTab(); }
 }
 
-function disableImages(matchPattern, reload) {  
+function disableImages(matchPattern, reload = false) {  
   setPagePermission(matchPattern, chrome.contentSettings.images, 'block', reload);
 }
 
-function enableImages(matchPattern, reload) {  
+function enableImages(matchPattern, reload = false) {  
   setPagePermission(matchPattern, chrome.contentSettings.images, 'allow', reload);
 }
 
-function disableScripts(matchPattern, reload) {  
+function disableScripts(matchPattern, reload = false) {  
   setPagePermission(matchPattern, chrome.contentSettings.javascript, 'block', reload);
 }
 
-function enableScripts(matchPattern, reload) {  
+function enableScripts(matchPattern, reload = false) {  
   setPagePermission(matchPattern, chrome.contentSettings.javascript, 'allow', reload);
 }
 
-function disableStyles(tabID){
+function disableStyles(tabID, callback = null){
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     chrome.tabs.sendMessage(tabs[0].id, {command: 'modify_styles', setting: 'disable'}, 
     function(response) {
-      //alert(response.numStyles);
+      callback
     });
   });
 }
 
-function enableStyles(tabID){
+function enableStyles(tabID, callback){
   chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     chrome.tabs.sendMessage(tabID, {command: 'modify_styles', setting: 'enable'}, 
     function(response) {
-      //alert(response.numStyles);
+      callback
     });
   });
 }
@@ -237,6 +238,7 @@ function popup(){
   window.open("popup.html");
 }
 
+/*
 function toggleScripts(){
   if(scripts) disableScripts();
   else       enableScripts();
@@ -254,6 +256,8 @@ function toggleStyles(){
   else       enableStyles();
   styles = !styles;
 }
+
+*/
 
 function openOptions(){
   chrome.tabs.create({url: optionsURL});
@@ -283,9 +287,9 @@ function triggerWarning(){
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('toggleJS').onclick = toggleScripts;
+    /*document.getElementById('toggleJS').onclick = toggleScripts;
     document.getElementById('toggleIMG').onclick = toggleImages;
-    document.getElementById('toggleStyles').onclick = toggleStyles;
+    document.getElementById('toggleStyles').onclick = toggleStyles; */
     document.getElementById('timeData').onclick = popup;
     document.getElementById('openAnalytics').onclick = openAnalytics;
     document.getElementById('openOptions').onclick = openOptions;
